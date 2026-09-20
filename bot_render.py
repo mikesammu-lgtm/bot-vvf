7a8ea73a23c906752115fd779ecdc2694e1636d8
mikesammu-lgtm <mikesammu@gmail.com>
2026-09-20 12:14:41 +0200

    Update bot_render.py

--- bot_render.py
+++ bot_render.py
@@ -1,52 +1,5 @@
-New logs in Live Tail
-
-Webhook deleted
-ArcGIS RAW status=200 len=103
-ArcGIS RAW preview={"error":{"code":499,"message":"Token Required","messageCode":"GWM_0003","details":["Token Required"]}}
-ArcGIS ERROR JSON: {'code': 499, 'message': 'Token Required', 'messageCode': 'GWM_0003', 'details': ['Token Required']}
-ArcGIS ALL -> 0 interventi
-ArcGIS RAW status=200 len=103
-ArcGIS RAW preview={"error":{"code":499,"message":"Token Required","messageCode":"GWM_0003","details":["Token Required"]}}
-ArcGIS ERROR JSON: {'code': 499, 'message': 'Token Required', 'messageCode': 'GWM_0003', 'details': ['Token Required']}
-ArcGIS ALL -> 0 interventi
-CB 729488977 -> raggio_vicini
-ArcGIS RAW status=200 len=103
-ArcGIS RAW preview={"error":{"code":499,"message":"Token Required","messageCode":"GWM_0003","details":["Token Required"]}}
-ArcGIS ERROR JSON: {'code': 499, 'message': 'Token Required', 'messageCode': 'GWM_0003', 'details': ['Token Required']}
-ArcGIS ALL -> 0 interventi
-ArcGIS RAW status=200 len=103
-ArcGIS RAW preview={"error":{"code":499,"message":"Token Required","messageCode":"GWM_0003","details":["Token Required"]}}
-ArcGIS ERROR JSON: {'code': 499, 'message': 'Token Required', 'messageCode': 'GWM_0003', 'details': ['Token Required']}
-ArcGIS ALL -> 0 interventi
-ArcGIS RAW status=200 len=103
-ArcGIS RAW preview={"error":{"code":499,"message":"Token Required","messageCode":"GWM_0003","details":["Token Required"]}}
-ArcGIS ERROR JSON: {'code': 499, 'message': 'Token Required', 'messageCode': 'GWM_0003', 'details': ['Token Required']}
-ArcGIS ALL -> 0 interventi
-ArcGIS RAW status=200 len=103
-ArcGIS RAW preview={"error":{"code":499,"message":"Token Required","messageCode":"GWM_0003","details":["Token Required"]}}
-ArcGIS ERROR JSON: {'code': 499, 'message': 'Token Required', 'messageCode': 'GWM_0003', 'details': ['Token Required']}
-ArcGIS ALL -> 0 interventi
-ArcGIS RAW status=200 len=103
-ArcGIS RAW preview={"error":{"code":499,"message":"Token Required","messageCode":"GWM_0003","details":["Token Required"]}}
-ArcGIS ERROR JSON: {'code': 499, 'message': 'Token Required', 'messageCode': 'GWM_0003', 'details': ['Token Required']}
-ArcGIS ALL -> 0 interventi
-ArcGIS RAW status=200 len=103
-ArcGIS RAW preview={"error":{"code":499,"message":"Token Required","messageCode":"GWM_0003","details":["Token Required"]}}
-ArcGIS ERROR JSON: {'code': 499, 'message': 'Token Required', 'messageCode': 'GWM_0003', 'details': ['Token Required']}
-ArcGIS ALL -> 0 interventi
-127.0.0.1 - - [20/Sep/2026 07:37:05] "GET / HTTP/1.1" 200 -
-==> 
-==> ///////////////////////////////////////////////////////////
-==> 
-==> Available at your primary URL https://bot-vvf.onrender.com
-==> 
-==> ///////////////////////////////////////////////////////////
-Task was destroyed but it is pending!
-task: <Task pending name='Task-5' coro=<background_loop() done, defined at /opt/render/project/src/bot_render.py:98> wait_for=<Future pending cb=[Task.task_wakeup()]>>
-==> Deploying...
-==> Setting WEB_CONCURRENCY=1 by default, based on available CPUs in the instance
-==> Running 'python bot_render.py'
-No error handlers are registered, logging exception.
-Traceback (most recent call last):
-  File "/opt/render/project/src/.venv/lib/python3.14/site-packages/telegram/ext/_updater.py", line 753, in _network_loop_retry
-    if not await do_action():
+    127.0.0.1 - - [20/Sep/2026 07:37:05] "GET / HTTP/1.1" 200 -
+                               ^
+SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+==> Exited with status 1
+==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
